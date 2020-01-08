1a9592a339ad4356092a0679ad4d4fc806aefa34
Camen <camenpihor@gmail.com>
2020-01-08 14:12:13 -0600

    add back in problem test; remove matrix from ci_main

--- tests/it/goes/test_sequence_s3.py
+++ tests/it/goes/test_sequence_s3.py
@@ -4,21 +4,20 @@ from wildfire import goes
 
 
 def test_get_goes_sequence():
-    assert True
-    # satellite = "noaa-goes17"
-    # region = "M1"
-    # start_time_utc = datetime.datetime(2019, 10, 1, 10, 5)
-    # end_time_utc = datetime.datetime(2019, 10, 1, 11, 4)
-    # actual = goes.get_goes_sequence(
-    #     satellite=satellite,
-    #     region=region,
-    #     start_time_utc=start_time_utc,
-    #     end_time_utc=end_time_utc,
-    #     max_scans_per_hour=2,
-    # )
-    # assert isinstance(actual, goes.GoesSequence)
-    # assert actual.region == region
-    # assert actual.satellite == satellite
-    # assert actual.first_scan_utc >= start_time_utc
-    # assert actual.last_scan_utc <= end_time_utc
-    # assert len(actual) == 2
+    satellite = "noaa-goes17"
+    region = "M1"
+    start_time_utc = datetime.datetime(2019, 10, 1, 10, 5)
+    end_time_utc = datetime.datetime(2019, 10, 1, 11, 4)
+    actual = goes.get_goes_sequence(
+        satellite=satellite,
+        region=region,
+        start_time_utc=start_time_utc,
+        end_time_utc=end_time_utc,
+        max_scans_per_hour=2,
+    )
+    assert isinstance(actual, goes.GoesSequence)
+    assert actual.region == region
+    assert actual.satellite == satellite
+    assert actual.first_scan_utc >= start_time_utc
+    assert actual.last_scan_utc <= end_time_utc
+    assert len(actual) == 2
